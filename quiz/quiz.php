4d0b7d446e0186eceac0d4368c2b94b731987256
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-09-03 12:40:48 +0200

    updated quiz.php

--- quiz/quiz.php
+++ quiz/quiz.php
@@ -1,25 +1,23 @@
 <?php include 'private/includes/nav.php' ?>
-
 <link rel="stylesheet" href="public/assets/css/quiz.css">
 
-<button onclick="playAudio()" type="button">Play Music</button>
-<button onclick="pauseAudio()" type="button">Pause Music</button>
-
+<div class="musicbuttons">
+    <button onclick="playAudio()" type="button">Play Music</button>
+    <button onclick="pauseAudio()" type="button">Pause Music</button>
+</div>
 <div class="quiz-container">
     <div id="quiz"></div>
 </div>
 
 <audio id="myAudio1">
-    <source src="80s Synthwave Background Music for Videos.ogg" type="audio/ogg">
-    <source src="80s Synthwave Background Music for Videos.mp3" type="audio/mpeg">
+    <source src="public/assets/music/music.ogg" type="audio/ogg">
+    <source src="public/assets/music/music.mp3" type="audio/mpeg">
 </audio>
 <br>
-<button id="previous">Previous Question</button>
-<button id="next">Next Question</button>
-<button id="submit">Submit Quiz</button>
+    <button id="previous">Previous Question</button>
+    <button id="next">Next Question</button>
+    <button id="submit">Submit Quiz</button>
 <div id="results"></div>
-
-
 
 <script src="public/assets/js/quiz.js"></script>
 <script src="public/assets/js/music.js"></script>
