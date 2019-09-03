9cc6e0133d235a90cadf62ee4aff4f17615788d2
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-09-03 09:17:36 +0200

    ghost link
    redirected the ghost link to a working link

--- quiz/index.php
+++ quiz/index.php
@@ -1,7 +1,7 @@
 <?php include 'private/includes/nav.php'; ?>
 
 <div class="container">
-    <span class="start"><a href="/">START</a></span>
+    <span class="start"><a href="quiz.php">START</a></span>
 </div>
 
 <?php include 'private/includes/footer.php'; ?>
