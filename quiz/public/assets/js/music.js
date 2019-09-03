0734425c16744a4889ff6ad17cd9488cc7b120c7
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-09-03 09:55:12 +0200

    created music.js
    i removed the js from quiz.php and made a own file for it

---
+++ quiz/public/assets/js/music.js
@@ -0,0 +1,9 @@
+var x = document.getElementById("myAudio1");
+
+function playAudio() {
+  x.play();
+}
+
+function pauseAudio() {
+  x.pause();
+}
--- quiz/quiz.php
+++ quiz/quiz.php
@@ -1,36 +1,27 @@
-<?php include 'private/includes/nav.php';?>
+<?php include 'private/includes/nav.php' ?>
 
-<?php 
-?>
+<link rel="stylesheet" href="public/assets/css/quiz.css">
+
 <button onclick="playAudio()" type="button">Play Music</button>
 <button onclick="pauseAudio()" type="button">Pause Music</button>
 
 <div class="quiz-container">
-  <div id="quiz"></div>
+    <div id="quiz"></div>
 </div>
 
 <audio id="myAudio1">
-  <source src="80s Synthwave Background Music for Videos.ogg" type="audio/ogg">
-  <source src="80s Synthwave Background Music for Videos.mp3" type="audio/mpeg">
-  Your browser does not support the audio element.
+    <source src="80s Synthwave Background Music for Videos.ogg" type="audio/ogg">
+    <source src="80s Synthwave Background Music for Videos.mp3" type="audio/mpeg">
 </audio>
 <br>
 <button id="previous">Previous Question</button>
 <button id="next">Next Question</button>
 <button id="submit">Submit Quiz</button>
 <div id="results"></div>
-<script src="quiz.js"></script>
-<link href="style.css" rel="stylesheet">
-<script>
-var x = document.getElementById("myAudio1");
 
-function playAudio() {
-  x.play();
-}
 
-function pauseAudio() {
-  x.pause();
-}
-</script>
 
-<?php include 'private/includes/footer.php';?>
+<script src="public/assets/js/quiz.js"></script>
+<script src="public/assets/js/music.js"></script>
+
+<?php include 'private/includes/footer.php' ?>
