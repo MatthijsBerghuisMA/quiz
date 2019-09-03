4813a1b8255672f2da382c924dff153b75bb36a1
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-09-03 09:02:21 +0200

    nav and footer
    the nav and the footer for our website

---
+++ quiz/private/includes/nav.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="public/assets/css/style.css">
+    <title>QUIZ</title>
+</head>
+<body>
+    <nav>
+        <span>QUI</span><span class="z">Z</span>
+    </nav>
