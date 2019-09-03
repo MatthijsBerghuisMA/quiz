a8f83f2800b5908d74f25efc8984ad49dea0ce5a
Matthijs Berghuis <43674789+MatthijsBerghuisMA@users.noreply.github.com>
2019-09-03 08:55:57 +0200

    Add files via upload

---
+++ quiz/index.php
@@ -0,0 +1,8 @@
+<?php include 'private/includes/nav.php'; ?>
+
+<div class="container">
+    <span class="start"><a href="/">START</a></span>
+</div>
+
+<?php include 'private/includes/footer.php'; ?>
+
