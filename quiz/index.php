dd225011a32e2cb86b59744be47688922e4e9013
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-09-04 16:02:19 +0200

    fixed bug

--- quiz/index.php
+++ quiz/index.php
@@ -1,1 +1,7 @@
-<html><body><center>Hier komt <b>matthijsberghuis.com</b>.</body></html>
+<?php include 'private/includes/nav.php'; ?>
+
+<div class="container">
+    <span class="start"><a href="quiz.php">START</a></span>
+</div>
+
+<?php include 'private/includes/footer.php'; ?>
